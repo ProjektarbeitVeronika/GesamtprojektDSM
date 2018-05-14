2fc715f990ac8608c5f876c7b8e83c9397b77b11
Seidel <vseidel@hs-ulm.de>
2018-05-14 13:18:08 +0200

    push files

---
+++ src/jUnitTests/mainMethode.java
@@ -0,0 +1,10 @@
+package jUnitTests;
+
+public class mainMethode {
+
+	public static void main(String[] args) {
+		Gui gui = new Gui();
+
+	}
+
+}
---
+++ testsrc/jUnitTests/SuperTestTest.java
@@ -0,0 +1,28 @@
+package jUnitTests;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SuperTestTest {
+	
+	private SuperTest classUnderTest;
+	
+	@Before
+	public void setUp() throws Exception {
+		classUnderTest = new SuperTest();
+		
+	}
+
+	@Test
+	public void testCalculate() {
+		double initialValue = 1.1;
+		
+		BigDecimal expectedValue = BigDecimal.valueOf(1.1*100 / Math.PI);
+	
+		assertTrue(expectedValue.equals(classUnderTest.calculate(initialValue)));
+	}
+}
