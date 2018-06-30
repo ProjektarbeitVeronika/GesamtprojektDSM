4e5ed6d5781bb6d54cb09b60aa1db4eb2b537da1
Veronika Seidel <vseidel@mail.hs-ulm.de>
2018-06-30 14:49:21 +0200

    main

--- src/mainMethod/main1.java
+++ src/mainMethod/main1.java
@@ -6,7 +6,7 @@ import algorithm.*;
 import inputOutput.*;
 import userinterface.*;
 
-public class main {
+public class main1 {
 
 	public static void main(String[] args) {
 		Gui myGui = new Gui();
